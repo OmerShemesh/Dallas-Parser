bc0d0c85714577f782a972b6280d064d21642ac2
Omer Shemsheyan <omershemesh95@gmail.com>
2016-12-28 17:43:56 +0200

    added parse_template && parse_vm

--- db_parser.py
+++ db_parser.py
@@ -13,13 +13,22 @@ db = mongo_client.dallas
 general_info_parser = GeneralInfoParser(cursor)
 
 cluster_collection = db.cluster
+template_collection = db.template
 host_collection = db.host
+vm_collection = db.vm
 
 
-cluster_collection.insert_one(general_info_parser.parse_cluster())
+for cluster in general_info_parser.parse_cluster():
+    cluster_collection.insert_one(cluster)
+
+for template in general_info_parser.parse_template():
+    template_collection.insert_one(template)
 
 for host in general_info_parser.parse_hosts():
     host_collection.insert_one(host)
+
+for vm in general_info_parser.parse_vms():
+    vm_collection.insert_one(vm)
 
 conn.close()
 cursor.close()
--- parsers.py
+++ parsers.py
@@ -1,28 +1,79 @@
-
 class GeneralInfoParser:
     def __init__(self, cursor):
         self.cursor = cursor
-        self.cursor.execute('SELECT * FROM vds')
-        self.vds_list = self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM vds")
+        self.vds_db_list = self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM vms")
+        self.vms_db_list = self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM cluster_view")
+        self.clusters_db_list = self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM vm_templates_view")
+        self.templates_db_list = self.cursor.fetchall()
+
+    def parse_template(self):
+        templates_list = []
+        for template in self.templates_db_list:
+            if template['name'] != "Blank":
+                template_dict = {
+                    '_id': template['vmt_guid'],
+                    'template_name': template['name'],
+                    'cluster_id': template['cluster_id']
+                }
+                templates_list.append(template_dict)
+        return templates_list
 
     def parse_cluster(self):
 
-        cluster_dict = {'_id': self.vds_list[0]['cluster_id'], 'cluster_name': self.vds_list[0]['cluster_name'],
-                        'hosts': []}
+        clusters_list = []
+        for cluster in self.clusters_db_list:
+            cluster_dict = {
+                '_id': cluster['cluster_id'],
+                'cluster_name': cluster['name'],
+                'ovirt_compatibility_version': cluster['compatibility_version'],
+                'hosts': [],
+                'vms': []}
 
-        for vds_host in self.vds_list:
-            cluster_dict['hosts'].append(vds_host['vds_id'])
-        return cluster_dict
+            for vds_host in self.vds_db_list:
+                cluster_dict['hosts'].append(vds_host['vds_id'])
+
+            for vm in self.vms_db_list:
+                if vm['cluster_id'] == cluster_dict['_id']:
+                    cluster_dict['vms'].append(vm['vm_guid'])
+
+            clusters_list.append(cluster_dict)
+
+            return clusters_list
 
     def parse_hosts(self):
         hosts_list = []
-        for vds_host in self.vds_list:
-            hosts_list.append(
-                {
-                    '_id': vds_host['vds_id'],
-                    'cpu_model': vds_host['cpu_model'],
-                    'host_name': vds_host['vds_name'],
-                    'host_ip': vds_host['host_name']
-                })
+        for vds_host in self.vds_db_list:
+            host_dict = {
+                '_id': vds_host['vds_id'],
+                'cpu_model': vds_host['cpu_model'],
+                'host_name': vds_host['vds_name'],
+                'host_ip': vds_host['host_name'],
+                'mem_size': vds_host['physical_mem_mb'],
+                'running_vms': []
+
+            }
+
+            for vm in self.vms_db_list:
+                if vm['run_on_vds'] == host_dict['_id']:
+                    host_dict['running_vms'].append(vm['vm_guid'])
+
+            hosts_list.append(host_dict)
 
         return hosts_list
+
+    def parse_vms(self):
+
+        vms_list = []
+        for vm in self.vms_db_list:
+            vms_list.append(
+                {
+                    '_id': vm['vm_guid'],
+                    'vm_name': vm['vm_name'],
+                    'mem_size': vm['mem_size_mb'],
+                })
+
+        return vms_list
